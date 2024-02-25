08b0dae348fa65db40c9af87ea40ee72aed50cb8
ereinha3 <ethanreinhart@gmail.com>
2024-02-24 17:10:00 -0800

    Scraped all images of matches between the dataset and appended to a new folder called Images

--- src/scraper.py
+++ src/scraper.py
@@ -1,5 +1,9 @@
 import csv
 import requests
+import sys
+sys.path.append("..")
+from docs.breeds_dict import breeds
+from docs.dog_pics import image_urls
 
 # Using CSV reader to parse file
 with open('../docs/dog-stats.csv', newline='\n') as csvfile:
@@ -18,7 +22,7 @@ with open('../docs/dog-stats.csv', newline='\n') as csvfile:
         else:
             for index in range(len(row)):
                 if index == 0:
-                    f.write('\t"{}" : '.format(row[index]))
+                    f.write('\t"{}" : '.format(row[index].lower()))
                     f.write("{\n")
                 else:
                     try:
@@ -34,7 +38,7 @@ with open('../docs/dog-stats.csv', newline='\n') as csvfile:
 
 with open("../docs/dog-names-and-pics.csv", newline = '\n') as csvfile:
     reader = csv.reader(csvfile, delimiter = ',')
-    f = open('../docs/dog-pics.py', 'w')
+    f = open('../docs/dog_pics.py', 'w')
     f.write("image_urls = {\n")
     first = 1
     for row in reader:
@@ -42,6 +46,16 @@ with open("../docs/dog-names-and-pics.csv", newline = '\n') as csvfile:
             first = 0
         else:
             if row[7]:
-                f.write('\t"{}" : "{}",\n'.format(row[1], row[7]))
+                f.write('\t"{}" : "{}",\n'.format(row[1].lower(), row[7]))
     f.write("}")
     f.close()
+
+matches = []
+for breed in image_urls.keys():
+    if breed in breeds.keys():
+        matches.append(breed)
+for match in matches:
+    res = requests.get(image_urls[match])
+    file_name = "../docs/images/" + match.replace(' ', '_') + ".jpg"
+    with open(file_name, "wb") as image_file:
+        image_file.write(res.content)
