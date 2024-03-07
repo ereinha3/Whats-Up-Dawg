b8c3fc99252bad74ffc0aab9592cf85d14c53be6
ereinha3 <ethanreinhart@gmail.com>
2024-03-06 17:55:04 -0800

    Updated match list to reflect matches between dog data and images only

--- src/data/scraper.py
+++ src/data/scraper.py
@@ -1,16 +1,16 @@
 import csv
 import requests
 import sys
-sys.path.append("..")
-from docs.breeds_dict import breeds
+sys.path.append("../..")
+from breeds_dict import breeds
 from docs.dog_pics import image_urls
 
 # Using CSV reader to parse file
-with open('../docs/dog-stats.csv', newline='\n') as csvfile:
+with open('../../docs/dog-stats.csv', newline='\n') as csvfile:
     reader = csv.reader(csvfile, delimiter=',', quotechar='"')
     headers = ["breed"]
     first = 1
-    f = open('../docs/breeds_dict.py', 'w')
+    f = open('breeds_dict.py', 'w')
     f.write("breeds = {\n")
     for row in reader:
         if first:
@@ -18,7 +18,6 @@ with open('../docs/dog-stats.csv', newline='\n') as csvfile:
                 if ele!='':
                     headers.append("{}".format(ele))
             first = 0
-            print(headers)
         else:
             for index in range(len(row)):
                 if index == 0:
@@ -29,16 +28,17 @@ with open('../docs/dog-stats.csv', newline='\n') as csvfile:
                         parameter = round(float(row[index]),2)
                     except:
                         parameter = "'{}'".format(str(row[index]).replace("'", '"'))
-                    print(parameter)
                     f.write("\t\t'{}': {},\n".format(headers[index], parameter))
 
             f.write("\t},\n")
     f.write("}")
     f.close()
 
-with open("../docs/dog-names-and-pics.csv", newline = '\n') as csvfile:
+print("Finished getting breed info...")
+
+with open("../../docs/dog-names-and-pics.csv", newline = '\n') as csvfile:
     reader = csv.reader(csvfile, delimiter = ',')
-    f = open('../docs/dog_pics.py', 'w')
+    f = open('../../docs/dog_pics.py', 'w')
     f.write("image_urls = {\n")
     first = 1
     for row in reader:
@@ -50,12 +50,25 @@ with open("../docs/dog-names-and-pics.csv", newline = '\n') as csvfile:
     f.write("}")
     f.close()
 
+print("Finished writing dog image locations...")
+
 matches = []
 for breed in image_urls.keys():
     if breed in breeds.keys():
         matches.append(breed)
+
+print("Found matches...")
+
+f = open("matches.py", 'w')
+f.write("matches = [")
 for match in matches:
-    res = requests.get(image_urls[match])
-    file_name = "../docs/images/" + match.replace(' ', '_') + ".jpg"
+    f.write(f'"{match}", ')
+    #res = requests.get(image_urls[match])
+    file_name = "../../docs/images/" + match.replace(' ', '_') + ".jpg"
+'''
     with open(file_name, "wb") as image_file:
         image_file.write(res.content)
+'''
+f.write(']')
+f.close()
+
