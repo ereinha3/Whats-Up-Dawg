5cf91909a151dca4aaecc5b26db2345d26e47690
ereinha3 <ethanreinhart@gmail.com>
2024-03-06 18:46:48 -0800

    Negative now

--- src/data/afflictions.py
+++ src/data/afflictions.py
@@ -1,31 +1,40 @@
 from random import *
 afflictions_dictionary = {
     0: 
-        {},
+        {
+          "dog_ate_pizza": {
+            "persistent": False, #until treatment
+            "indefinite": False, #until death
+            "cure": { "cost": 150,  "work": 4, },
+            "health": -5,
+            "stress": -5,
+            "description": "Fleas cause anemia, and may carry eggs from other parasites, particularly tapeworms. Treatment involves medications, specialty shampoos and extermination of fleas from the environment.",
+        },  
+        },
     1:
         {
         "fleas": {
             "persistent": True, #until treatment
             "indefinite": False, #until death
-            "cure": { "cost": 150,  "work": 8, },
-            "harm": 5,
-            "stress": 5,
+            "cure": { "cost": lambda dog : 150,  "work": 8, },
+            "health": -5,
+            "stress": -5,
             "description": "Fleas cause anemia, and may carry eggs from other parasites, particularly tapeworms. Treatment involves medications, specialty shampoos and extermination of fleas from the environment.",
         },
         "ingrown_nail" : {
             "persistent": False,
             "indefinite": False,
             "cure": { "cost": lambda dog : 100,  "work": 1, }, 
-            "harm": 1,
-            "stress": 2,
+            "health": -1,
+            "stress": -2,
             "description": "Your dog has an ingrown nail on its paw.",
         },
         "bur_in_paw" : {
             "persistent": False,
             "indefinite": False,
             "cure": { "cost": lambda dog : 100,  "work": 1, }, 
-            "harm": 1,
-            "stress": 2,
+            "health": -1,
+            "stress": -2,
             "description": "Your dog has a burr in its paw.",
         },
         },
@@ -35,16 +44,16 @@ afflictions_dictionary = {
             "persistent": True,
             "indefinite": True,
             "cure": { "cost": lambda dog : 60,  "work": 1, }, #this is the recurrent cost... NOT the intitial cost. That will be in the event lookup 
-            "harm": 1,
-            "stress": 2,
+            "health": -1,
+            "stress": -2,
             "description": "Allergies are a lifelong condition. Your dog will need to recieve treatment for it's whole life.",
         },
         "obesity" : {
             "persistent": True,
             "indefinite": True,
             "cure": { "cost": lambda dog : 0,  "work": 10, }, #this is the recurrent cost... NOT the intitial cost. That will be in the event lookup 
-            "harm": 1,
-            "stress": 2,
+            "health": -3,
+            "stress": -2,
             "description": "Obesity is a lasting condition. You will need to walk you dog more frequenlty and feed better food.",
         },
         },
@@ -54,8 +63,8 @@ afflictions_dictionary = {
             "persistent": True,
             "indefinite": False,
             "cure": { "cost": lambda dog : 500 + 600*random.random(),  "work": 2, }, #WHEN SIZE is implemented, change cost to lambda dog : dog.get_size() to output between $500-1100
-            "harm": 10,
-            "stress": 8,
+            "health": -100,
+            "stress": -8,
             "description": "Heartworms is a very serious parasitic disease affecting the lungs, heart, and other organs. Will result in death if left untreated.",
         },
         },
